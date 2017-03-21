fade7ffebfc700a9e56e37369ff823c8acc89cb0
Vincent Cheung <coolingfall@gmail.com>
2017-03-21 10:48:54 +0800

    Add license for files

--- routine/src/main/java/com/anbillon/routine/Matcher.java
+++ routine/src/main/java/com/anbillon/routine/Matcher.java
@@ -1,11 +1,27 @@
+/*
+ * Copyright (C) 2017 Tourbillon Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.anbillon.routine;
 
 import android.content.Context;
 import android.content.Intent;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static com.anbillon.routine.Utils.resolveActivityInfo;
 import static com.anbillon.routine.Utils.resolveSchemeUrl;
@@ -55,7 +71,7 @@ public final class Matcher {
   }
 
   public static final class Builder {
-    private Map<String, Set<Class<?>>> pagesMap = new ConcurrentHashMap<>();
+    private Map<String, Set<Class<?>>> pagesMap = new LinkedHashMap<>();
 
     Builder() {
     }
@@ -70,7 +86,7 @@ public final class Matcher {
       if (pages != null && !pages.isEmpty()) {
         pages.add(page);
       } else {
-        HashSet<Class<?>> newPages = new HashSet<>();
+        LinkedHashSet<Class<?>> newPages = new LinkedHashSet<>();
         newPages.add(page);
         pagesMap.put(schemeUrl, newPages);
       }
